0e238aadfa18f2cadfa426228dc02d10dfb349ea
MDOsipov <dos12345671@gmail.com>
2022-10-13 14:43:25 +0300

    Fixed fruits generation

--- homework.c
+++ homework.c
@@ -1,6 +1,9 @@
 ﻿#include <windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/timeb.h>
+
+// Set metrics of the frame
 #define FRAME_WIDTH 800
 #define FRAME_HEIGHT 800
 #define FRAME_UPPER_Y 100
@@ -8,6 +11,7 @@
 #define FRAME_LEFT_X 200
 #define FRAME_RIGHT_X (FRAME_LEFT_X + FRAME_WIDTH)
 
+// Set size of objects
 #define SNACK_BLOCK_SIDE 20
 #define FRUIT_SIZE 20
 
@@ -32,6 +36,9 @@ typedef struct SSnake
     PObject parts;
 } TSnake;
 
+typedef enum { Up = 1, Down = 2, Right = 3, Left = 4 } Destination;
+
+// Define functions
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 Tpoint point(float x, float y);
 void WinInit();
@@ -49,9 +56,12 @@ void AddFruit(float xPos, float yPos);
 BOOL ObjectCollision(TObject o1, TObject o2);
 void DelFruits();
 void UpdateSpeed();
+void AddSnakeBlock();
 
 // Overall score
 int score = 0;
+int t = 0;
+
 
 RECT rect;
 // Create our snake
@@ -59,11 +69,10 @@ TSnake snake;
 // Other objects (fruits)
 PObject fruits = NULL;
 int fruitsCnt = 0; // The current number of fruits
+// Start new game flag
 BOOL startNewGame = FALSE;
-float snakeSpeed = 2;
-
-
-
+// Set the snake speed
+float snakeSpeed = 1.5;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR szCmdLine, int iCmdShow)
@@ -106,6 +115,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     // Create device context
     HDC dc = GetDC(hwnd);
+
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
     WinInit();
@@ -121,13 +131,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         }
         else
         {
+            t++;
             WinMove();
+            for (int i = 0; i < fruitsCnt; i++)
+            {
+                if (ObjectCollision(*(fruits + i), *(snake.parts)) && (fruits + i)->isDel != TRUE)
+                {
+                    (fruits + i) -> isDel = TRUE;
+                    score++;
+                }
+            }
+           
             WinShow(dc);
+            DelFruits();
             CheckBoundaries();
-            Sleep(5);
         }
     }
-
     ReleaseDC(hwnd, dc);
     return msg.wParam;
 }
@@ -148,9 +167,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 
         GetClientRect(hwnd, &rect);
 
-
-        DrawText(hdc, TEXT("Hello, Windows 98!"), -1, &rect,
-            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
         EndPaint(hwnd, &ps);
         return 0;
 
@@ -175,15 +191,21 @@ void WinInit()
 {
     startNewGame = FALSE;
     fruitsCnt = 0;
+    score = 0;
+    if (!fruits)
+    {
+        free(fruits);
+    }
     fruits = NULL;
     snakeSpeed = 2;
+    free(snake.parts);
     SnakeInit();
 }
 
 void SnakeInit()
 {
     snake.length = 1;
-    snake.parts = ((TObject*)malloc(sizeof(TObject))) ? (TObject*)malloc(sizeof(TObject)) : NULL;
+    snake.parts = (TObject*)malloc(sizeof(TObject));
     ObjectInit(snake.parts, FRAME_LEFT_X + (FRAME_RIGHT_X - FRAME_LEFT_X) / 2, FRAME_UPPER_Y + (FRAME_LOWER_Y - FRAME_UPPER_Y) / 2, SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, 's');
 }
 
@@ -191,7 +213,6 @@ void ObjectInit(TObject* obj, float xPos, float yPos, float width, float height,
 {
     int choose = 0;
     srand((int)time(NULL));
-
 
     obj->pos = point(xPos, yPos);
     obj->size = point(width, height);
@@ -222,7 +243,7 @@ void ObjectInit(TObject* obj, float xPos, float yPos, float width, float height,
     }
     else if (objType == 'f')
     {
-        COLORREF color = RGB(rand() % 255, rand() % 255, rand() % 255);
+        COLORREF color = RGB(100 + rand() % 155, 100 + rand() % 155, 100 + rand() % 155);
         obj->brush = color;
     }
 }
@@ -235,16 +256,19 @@ void WinMove()
     }
 
     SnakeControl();
+    // Move head
     ObjectMove(snake.parts);
+    // Move tail
+    for (int i = 1; i < snake.length; i++)
+    {
+        ObjectMove(snake.parts + i);
+    }
     GenerateFruit();
-    DelFruits();
 }
 
 void SnakeControl()
 {
-    // snake.speed.x = 0;
-    // snake.speed.y = 0;
-
+ 
     if (GetKeyState('W') < 0)
     {
         snake.parts[0].speed.y = -snakeSpeed;
@@ -286,16 +310,31 @@ void WinShow(HDC dc)
     
     FrameShow(memDC);
     ObjectShow(*snake.parts, memDC);
+
+    for (int i = 1; i < snake.length; i++)
+    {
+        ObjectShow(*(snake.parts + i), memDC);
+    }
+
     for (int i = 0; i < fruitsCnt; i++)
     {
         ObjectShow(fruits[i], memDC);
-        if (ObjectCollision(fruits[i], *(snake.parts)))
-        {
-            score++;
-            fruits[i].isDel = TRUE;
-        }
-        
     }
+
+    wchar_t bufferForScore[50];
+    int buf_len = 0;
+
+    buf_len = wsprintf(bufferForScore, TEXT("Score = %i"), score);
+    TextOut(memDC, 400, 50, bufferForScore, buf_len);
+
+    buf_len = wsprintf(bufferForScore, TEXT("Fruits num = %i"), fruitsCnt);
+    TextOut(memDC, 600, 50, bufferForScore, buf_len);
+
+    buf_len = wsprintf(bufferForScore, TEXT("Time = %i"), t);
+    TextOut(memDC, 800, 50, bufferForScore, buf_len);
+
+    buf_len = wsprintf(bufferForScore, TEXT("Milisecs = %i"), (int)time(NULL) * 1000 + getMiliseconds());
+    TextOut(memDC, 950, 50, bufferForScore, buf_len);
 
     BitBlt(dc, 0, 0, rect.right - rect.left, rect.bottom - rect.top, memDC, 0, 0, SRCCOPY);
     DeleteDC(memDC);
@@ -350,27 +389,27 @@ void CheckBoundaries()
 
 void GenerateFruit()
 {
-    srand(time(NULL));
+    srand((int)time(NULL) * 1000 + getMiliseconds());
     int xPos = FRAME_LEFT_X + rand() % FRAME_WIDTH;
     int yPos = FRAME_UPPER_Y + rand() % FRAME_HEIGHT;   
+    BOOL check = FALSE;
 
-    int k = rand() % 7;
+    Sleep(1);
+
+    int k = rand() % 300;
     if (k == 1)
     {
-        AddFruit(xPos, yPos);
-    }
-    if (k == 2)
-    {
-        AddFruit(xPos, yPos);
+        fruitsCnt++;
+        check = TRUE;
+        AddFruit(xPos, yPos, fruitsCnt);
     }
 }
 
-void AddFruit(float xPos, float yPos)
+void AddFruit(float xPos, float yPos, int frt_cnt)
 {
-    fruitsCnt++;
-    if (fruits = realloc(fruits, sizeof(*fruits) * fruitsCnt))
+    if (fruits = realloc(fruits, sizeof(*fruits) * frt_cnt))
     {
-       ObjectInit(fruits + fruitsCnt - 1, xPos, yPos, FRUIT_SIZE, FRUIT_SIZE, 'f');
+       ObjectInit(fruits + frt_cnt - 1, xPos, yPos, FRUIT_SIZE, FRUIT_SIZE, 'f');
     }
     else
     {
@@ -380,6 +419,10 @@ void AddFruit(float xPos, float yPos)
 
 BOOL ObjectCollision(TObject o1, TObject o2)
 {
+    if (o1.isDel == TRUE)
+    {
+        return FALSE;
+    }
     return ((o1.pos.x + o1.size.x) > o2.pos.x) && (o1.pos.x < (o2.pos.x + o2.size.x)) &&
         ((o1.pos.y + o1.size.y) > o2.pos.y) && (o1.pos.y < (o2.pos.y + o2.size.y));
 }
@@ -389,7 +432,7 @@ void DelFruits()
     int del_cntr = 0;
     for (int i = 0; i < fruitsCnt; i++)
     {
-        if (fruits[i].isDel)
+        if ((fruits + i) -> isDel == TRUE)
         {
             del_cntr++;
             fruitsCnt--;
@@ -397,14 +440,74 @@ void DelFruits()
             fruits = realloc(fruits, sizeof(*fruits) * fruitsCnt);
         }
     }
-    Sleep(5);
-    for (int i = 0; i < del_cntr; i++)
-    {
+    // Sleep(5);
+    //for (int i = 0; i < del_cntr; i++)
+    //{
+        // AddSnakeBlock();
         UpdateSpeed();
-    }
+    //}
 }
 
 void UpdateSpeed()
 {
-    snakeSpeed += 0.02;
+    snakeSpeed += 0.001;
 }
+
+void AddSnakeBlock()
+{
+    int xPrevPos = snake.parts[snake.length - 1].pos.x;
+    int yPrevPos = snake.parts[snake.length - 1].pos.y;
+    Tpoint prevSpeed = snake.parts[snake.length - 1].speed;
+
+    snake.length++;
+    snake.parts = (TObject*)realloc(snake.parts, snake.length * sizeof(TObject));
+
+    switch (FindDestination(snake.parts))
+    {
+        case Right:
+            snake.parts->speed = prevSpeed;
+            ObjectInit(snake.parts + snake.length - 1, xPrevPos - SNACK_BLOCK_SIDE, yPrevPos, SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, 't');
+            break;
+        case Left:
+            snake.parts->speed = prevSpeed;
+            ObjectInit(snake.parts + snake.length - 1, xPrevPos + SNACK_BLOCK_SIDE, yPrevPos, SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, 't');
+            break;
+        case Up:
+            snake.parts->speed = prevSpeed;
+            ObjectInit(snake.parts + snake.length - 1, xPrevPos, yPrevPos + SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, 't');
+            break;
+        case Down:
+            snake.parts->speed = prevSpeed;
+            ObjectInit(snake.parts + snake.length - 1, xPrevPos, yPrevPos - SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, SNACK_BLOCK_SIDE, 't');
+            break;
+    }
+
+}
+
+Destination FindDestination(PObject obj)
+{
+    if (obj->speed.x > 0)
+    {
+        return Right;
+    }
+    else if (obj->speed.x < 0)
+    {
+        return Left;
+    }
+    else if (obj->speed.y > 0)
+    {
+        return Down;
+    }
+    else if (obj->speed.y < 0)
+    {
+        return Up;
+    }
+}
+
+int getMiliseconds() 
+{
+    struct _timeb timebuffer;
+    _ftime(&timebuffer);
+    return (int)timebuffer.millitm; 
+}
+
